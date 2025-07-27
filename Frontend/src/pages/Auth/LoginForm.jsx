89c80407ea835149eaad1bc91f58d6059f771f91
Aditya Kumar Singh <singhaditya6678@gmail.com>
2025-07-27 23:47:30 +0530

    Signup made , problems in uploading picture , fix that later

--- Frontend/src/pages/Auth/LoginForm.jsx
+++ Frontend/src/pages/Auth/LoginForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import AuthLayout from '../../components/layout/AuthLayout'
 import {Link, useNavigate} from "react-router-dom"
 import AuthInput from '../../components/Input/AuthInput'
+import { validateEmail } from '../../utils/helper'
 function LoginForm() {
 
   const [email,setEmail] = useState("")
@@ -9,8 +10,25 @@ function LoginForm() {
   const [error,setError] = useState(null);
 
   const navigate = useNavigate();
-  const handleSubmit = async() =>{
+  const handleLogin = async(e) =>{
+      e.preventDefault();
 
+      if(!validateEmail(email)){
+        setError("Please enter a valid email address.")
+        return ;
+      }
+      if(!password){
+        setError("Please enter a password.");
+        return;
+      }
+      setError("");
+
+      //Login API
+      try {
+        
+      } catch (error) {
+        
+      }
   }
   return (
     <AuthLayout>
@@ -19,7 +37,7 @@ function LoginForm() {
       <p className='text-sm text-gray-700 mt-[5px] mb-6 '>
           Please Enter your details to log in
       </p>
-      <form onSubmit={handleSubmit} className="space-y-5">
+      <form onSubmit={handleLogin} className="space-y-5">
         <AuthInput 
           value = {email}
           onChange = {(e)=>setEmail(e.target.value)}
@@ -30,7 +48,7 @@ function LoginForm() {
           value = {password}
           onChange = {(e)=>setPassword(e.target.value)}
           label = "Password"
-          placeholder = "***"
+          placeholder = "Min 8 Characters"
           type = "password"/>
           {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
           <button type = "submit" className='w-full flex justify-center bg-purple-700 hover:bg-purple-600 text-white py-3 rounded-lg text-sm font-semibold tracking-wide transition-all duration-300'>
--- Frontend/src/pages/Auth/SignupForm.jsx
+++ Frontend/src/pages/Auth/SignupForm.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import AuthLayout from "../../components/layout/AuthLayout";
+import AuthInput from "../../components/Input/AuthInput";
+import { validateEmail, validatePassword } from "../../utils/helper";
 function SignupForm() {
+  const [name, setName] = useState("");
+  const [profilePhoto, setProfilePhoto] = useState("");
+  const [dateOfBirth, setDOB] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please Enter a valid email address");
+      return;
+    }
+    if (!validatePassword(password)) {
+      setError(
+    "Invalid Password:\n" +
+    "Your Password must have minimum :\n" +
+    "• 1 lowercase letter\n" +
+    "• 1 uppercase letter\n" +
+    "• One digit\n" +
+    "• At least 8 characters"
+  );
+      return;
+    }
+    if (password !== confirmPassword) {
+      setError("Password and Confirm Password should be same");
+      return;
+    }
+    setError("");
+
+    //api call for Signup
+    try {
+    } catch (error) {}
+  };
   return (
-    <div>SignupForm</div>
-  )
+    <AuthLayout>
+      <div className="w-full lg:w-[70%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center ">
+        <h3 className="text-xl font-semibold text-black"></h3>
+        <p className="text-sm text-gray-700 mt-[5px] mb-6 ">
+          Please Enter your details to Sign In.
+        </p>
+        <form onSubmit={handleSignup} className="space-y-5">
+          <AuthInput
+            value={name}
+            onChange={(e) => setFirstName(e.target.value)}
+            label="Name"
+            placeholder="john"
+            type="text"
+          />
+          <AuthInput
+            value={profilePhoto}
+            onChange={(e) => setProfilePhoto(e.target.value)}
+            label="Upload your Profile Photo"
+            placeholder=""
+            type="file"
+          />
+          <AuthInput
+            value={dateOfBirth}
+            onChange={(e) => setDOB(e.target.value)}
+            label="DOB"
+            placeholder="DD/MM/YY"
+            type="date"
+          />
+          <AuthInput
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email address"
+            placeholder="john@example.com"
+            type="text"
+          />
+          <AuthInput
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="password"
+          />
+          <AuthInput
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            label="Confirm Password"
+            placeholder="Min 8 Characters"
+            type="password"
+          />
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+          <button
+            type="submit"
+            className="w-full flex justify-center bg-purple-700 hover:bg-purple-600 text-white py-3 rounded-lg text-sm font-semibold tracking-wide transition-all duration-300"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 }
 
-export default SignupForm
+export default SignupForm;
