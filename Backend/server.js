b3fb676dc9fbfa76eacbfa1fd956131333bc7edc
Aditya Kumar Singh <singhaditya6678@gmail.com>
2025-08-13 00:50:01 +0530

    Trying vercel and render

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "http://localhost:5173" || "*",
+        origin: process.env.CLIENT_URL || "http://localhost:5173" || "*" || "https://your-vercel-frontend.vercel.app",
         methods: ["GET", "POST","PUT","DELETE"],
         allowedHeaders: ["Content-Type","Authorization"],
         credentials: true
--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: BASE_URL || import.meta.env.VITE_API_BASE_URL,
     timeout: 100000,
     headers:{
         "Content-Type":"application/json",
